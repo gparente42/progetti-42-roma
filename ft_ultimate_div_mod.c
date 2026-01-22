4a78a0567b106515f1a043890d26d74b9dd708b3
gparente42 <parentegiampaolo94@gmail.com>
2026-01-22 13:45:56 +0100

    progetto c01

---
+++ ft_ultimate_div_mod.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gparente <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/21 17:50:37 by gparente          #+#    #+#             */
+/*   Updated: 2026/01/21 18:34:22 by gparente         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_ultimate_div_mod(int *a, int *b)
+{
+	int	div;
+	int	mod;
+
+	div = *a / *b;
+	mod = *a % *b;
+	*a = div;
+	*b = mod;
+}
+/*
+int	main(void)
+{
+	int	a;
+	int	b;
+
+	ft_ultimate_div_mod(&a, &b);
+	write(1, "ciao\n", 5);
+	return (0);
+}
+*/
