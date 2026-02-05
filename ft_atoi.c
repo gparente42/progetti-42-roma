2bd0ca95f1bda544234d71d46843cbeae94d2b74
gparente42 <parentegiampaolo94@gmail.com>
2026-02-05 12:17:35 +0100

    Progetto C04
    progetto c04

---
+++ ft_atoi.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gparente <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/04 11:29:50 by gparente          #+#    #+#             */
+/*   Updated: 2026/02/04 11:29:53 by gparente         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] == ' ' || str[i] == '\f' || str[i] == '\n'
+		|| str[i] == '\r' || str[i] == '\t' || str[i] == '\v')
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = (result * 10) + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
+/*
+int	main(void)
+{
+	printf("%d\n", ft_atoi("grsr45464"));
+	printf("%d\n", ft_atoi("   -1234abc"));
+	printf("%d\n", ft_atoi("42"));
+	return (0);
+}
+*/
